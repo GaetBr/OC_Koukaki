23ed14da950aaebf6058ae4b772436301574f5fe
Gaëtan Brun <brungaetan@outlook.fr>
2023-09-27 17:05:02 +0200

    Création de la section nomination aux oscars

---
+++ wp-content/themes/foce-child/templates/oscar.php
@@ -0,0 +1,4 @@
+<section id="nomination">
+    <h4 class="nomination-title">Fleur d'oranger & chats errants</br> est nominé aux oscars Short</br> Film Animated de 2022 !</h4>
+    <img src="<?php echo get_stylesheet_directory_uri() . '../assets/images/oscar-nomination.png'; ?> " alt="logo oscars" class="nomination-image">
+</section>
