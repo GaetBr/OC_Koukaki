e98275ff62c5ec66233bc8a9dc2ac795641c3d3d
Gaëtan Brun <brungaetan@outlook.fr>
2023-10-03 10:31:30 +0200

    Ajout des images à l'article id place

--- wp-content/themes/foce-child/front-page.php
+++ wp-content/themes/foce-child/front-page.php
@@ -9,55 +9,25 @@ get_header();
             <video class="hero-video" autoplay loop muted playsinline>
                 <source src="<?php echo get_stylesheet_directory_uri() . '/assets/studio_koukaki-video_header.mp4'; ?>" type="video/mp4">
             </video>
-            <img class="parallax-title" src="<?php echo get_template_directory_uri() . '/assets/images/logo.png'; ?> " alt="logo Fleurs d'oranger & chats errants">
+            <div class="parallax-title">
+                <img src="<?php echo get_template_directory_uri() . '/assets/images/logo.png'; ?> " alt="logo Fleurs d'oranger & chats errants">
+            </div>
         </section>
         <section id="#story" class="story fade-in">
             <h2><span class="animate-section-title">L'histoire</span></h2>
             <article id="" class="story__article">
                 <p><?php echo get_theme_mod('story'); ?></p>
             </article>
-            <?php
-            $args = array(
-                'post_type' => 'characters',
-                'posts_per_page' => -1,
-                'meta_key'  => '_main_char_field',
-                'orderby'   => 'meta_value_num',
-
-            );
-            $characters_query = new WP_Query($args);
-            ?>
             <article id="characters">
-                <div class="main-character">
-                    <h3><span class="animate-section-title">Les personnages</span></h3>
-                    <?php
-                    $main_character = $characters_query->posts[0];
-                    echo '<figure>';
-                    echo get_the_post_thumbnail( $main_character->ID, 'full' );
-                    echo '<figcaption>'. $main_character->post_title . '</figcaption>';
-                    echo '</figure>';
-                    $characters_query->next_post();
-                    ?>
-                </div>
-                <div class="other-characters">
-                    <?php
-                    while ( $characters_query->have_posts() ) {
-                        $characters_query->the_post();
-                        echo '<figure>';
-                        echo get_the_post_thumbnail( get_the_ID(), 'full' );
-                        echo '<figcaption>';
-                        the_title();
-                        echo'</figcaption>';
-                        echo '</figure>';
-                    }
-                    ?>
-                </div>
+                <?php get_template_part('templates/characters-carousel'); ?>
             </article>
             <article id="place">
                 <div>
                     <h3><span class="animate-section-title">Le Lieu</span></h3>
                     <p><?php echo get_theme_mod('place'); ?></p>
                 </div>
-
+                <img src="<?php echo get_stylesheet_directory_uri() . '/assets/images/little_cloud.png'; ?> " alt="petit nuage" class="parallax-nuage little-cloud">
+                <img src="<?php echo get_stylesheet_directory_uri() . '/assets/images/big_cloud.png'; ?> " alt="gros nuage" class="parallax-nuage big-cloud">
             </article>
         </section>
 
