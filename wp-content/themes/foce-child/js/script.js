d8c9b0639173506819eec7493fc93c3870e336ab
Gaëtan Brun <brungaetan@outlook.fr>
2023-10-09 10:28:43 +0200

    Projet Fini

--- wp-content/themes/foce-child/js/script.js
+++ wp-content/themes/foce-child/js/script.js
@@ -49,27 +49,66 @@ window.addEventListener("load", handleScroll);
 
 /***** MENU BURGER *****/
 /***********************/
+// Fonction pour gérer l'animation des titres
+function handleLinkAnimation() {
+  const linkElements = document.querySelectorAll(".animate-section-link");
+
+  linkElements.forEach((linkElement) => {
+    // Ajoute la classe pour indiquer que le titre est visible
+    linkElement.classList.add("section-link-visible");
+
+    // Sélectionne chaque lettre dans le texte
+    const letters = linkElement.textContent.split("");
+
+    // Remplace le contenu de l'élément par des <span> pour chaque lettre
+    linkElement.innerHTML = letters
+      .map((letter) => `<span class="letter">${letter}</span>`)
+      .join("");
+
+    // Sélectionne toutes les lettres
+    const letterElements = linkElement.querySelectorAll(".letter");
+
+    // Animation avec GSAP pour que les lettres montent depuis le bas jusqu'à leur position initiale
+    gsap.fromTo(
+      letterElements,
+      { opacity: 0, y: "100" }, // Propriétés de départ
+      { opacity: 1, y: "0", stagger: 0.2, duration: 0.2, ease: "power4.out" } // Propriétés finales avec délai entre les lettres
+    );
+  });
+}
+
+// Fonction pour gérer l'ouverture du menu burger et déclencher l'animation des titres
+function handleMenuOpen() {
+  handleLinkAnimation();
+}
+
 var sidenav = document.getElementById("mySidenav");
 var openBtn = document.getElementById("openBtn");
 var burgerIcon = document.querySelector(".burger-icon");
 
 openBtn.onclick = function() {
-    if (sidenav.classList.contains("active")) {
-        closeNav();
-        burgerIcon.classList.remove("active");
-    } else {
-        openNav();
-        burgerIcon.classList.add("active");
-    }
+  if (sidenav.classList.contains("active")) {
+    closeNav();
+    burgerIcon.classList.remove("active");
+  } else {
+    openNav();
+    burgerIcon.classList.add("active");
+    handleMenuOpen();
+  }
 };
 
 function openNav() {
-    sidenav.classList.add("active");
+  sidenav.style.visibility = "visible";
+  sidenav.style.opacity = "1";
+  sidenav.classList.add("active");
 }
 
 function closeNav() {
-    sidenav.classList.remove("active");
+  sidenav.style.visibility = "hidden";
+  sidenav.style.opacity = "0";
+  sidenav.classList.remove("active");
 }
+
 /***** PARALLAX TITRE *****/
 /**************************/
 
@@ -105,7 +144,7 @@ var swiper = new Swiper('.swiper-container', {
   },
   effect: 'coverflow',
   grabCursor: true,
-  centeredSlides: false,
+  centeredSlides: true,
   slidesPerView: 3,
   slidesPerGroup: 1,
   spaceBetween: 10,
@@ -116,32 +155,44 @@ var swiper = new Swiper('.swiper-container', {
       modifier: 1,
       slideShadows: false,
   },
-}) 
+});
 
 /***** PARALLAX NUAGE *****/
 /**************************/
+// Fonction pour vérifier si un élément est dans la fenêtre visible
+function isElementInViewport(el) {
+  var rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
 var parallaxImages = document.querySelectorAll(".parallax-nuage");
+var articlePlace = document.getElementById("place");
 
-  window.addEventListener("scroll", function () {
+// Stocke les positions initiales des images
+var initialPositions = Array.from(parallaxImages).map(function (image) {
+  return image.getBoundingClientRect().left;
+});
+
+window.addEventListener("scroll", function () {
+  // Vérifie si l'article est visible à l'écran
+  if (isElementInViewport(articlePlace)) {
     // Vérifie si les deux images sont visibles à l'écran
     if (isElementInViewport(parallaxImages[0]) && isElementInViewport(parallaxImages[1])) {
       var scrollPosition = window.scrollY;
-      var displacement = -scrollPosition / 5;
 
       // Parcours toutes les images avec la classe parallax-nuage
-      parallaxImages.forEach(function (image) {
+      parallaxImages.forEach(function (image, index) {
+        var displacement = -(scrollPosition - initialPositions[index]) / 5;
+
+        // Limite la valeur de déplacement à un maximum de 300px dans les deux sens
+        displacement = Math.max(-300, Math.min(300, displacement));
+
         image.style.transform = "translateX(" + displacement + "px)";
       });
     }
-  });
-
-  // Fonction pour vérifier si un élément est dans la fenêtre visible
-  function isElementInViewport(el) {
-    var rect = el.getBoundingClientRect();
-    return (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-}
+  }
+});
